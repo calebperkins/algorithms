e9eafaa41955d34c0437bd4421317b35c801a748
Caleb Perkins <calebtperkins@gmail.com>
2015-02-24 13:52:11 -0800

    Small tweak to permutations

--- src/permutations.py
+++ src/permutations.py
@@ -1,8 +1,7 @@
 # Given an input sequence, generate all permutations of that sequence.
 def permute(sequence):
-    if len(sequence) <= 1:
-        for n in sequence:
-            yield [n]
+    if len(sequence) == 1:
+        yield sequence
     for i in range(len(sequence)):
         sequence[0], sequence[i] = sequence[i], sequence[0]
         for subseq in permute(sequence[1:]):
