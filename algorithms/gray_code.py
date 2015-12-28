15fef0f38503ab1d941d6b82af1a3bb09527163c
Caleb Perkins <calebtperkins@gmail.com>
2015-12-28 13:33:48 -0800

    Clean up gray code.

--- algorithms/gray_code.py
+++ algorithms/gray_code.py
@@ -5,33 +5,14 @@ https://en.wikipedia.org/wiki/Gray_code
 https://oj.leetcode.com/problems/gray-code/
 """
 
-# Recursive version
-
-
-def _gray_code(n):
-    if n == 0:
-        return [0]
-    last = _gray_code(n - 1)
-    l = len(last)
-    for i in range(l - 1, -1, -1):
-        x = last[i]
-        y = x | (1 << (n - 1))
-        last.append(y)
-    return last
-
-# Iterative version
-
-
-def gray_code(n):
+def gray_code(bits):
+    "Produce a gray code with the specified number of bits."
     m = 0
     code = [0]
-    length = 1
-    while m < n:
+    while m < bits:
         # reverse last sequence and prepend with bit 1
-        for i in range(length - 1, -1, -1):
-            x = code[i]
+        for x in reversed(code):
             y = x | (1 << m)
             code.append(y)
         m += 1
-        length *= 2
     return code
