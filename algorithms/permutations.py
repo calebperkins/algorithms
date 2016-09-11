8e4d533d6e138e65adaa05faa3d1bab4e1d3c70a
Caleb Perkins <calebtperkins@gmail.com>
2016-09-10 17:35:52 -0700

    Removed recursion from permutations.

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -1,12 +1,19 @@
 def permute(sequence):
     "Given an input sequence, generate all permutations of that sequence."
     if not sequence:
-        return
-    if len(sequence) == 1:
-        yield sequence
-        return
-    for subseq in permute(sequence[1:]):
-        for i in range(len(sequence)):
-            copy = list(subseq)
-            copy.insert(i, sequence[0])
-            yield copy
+        return []
+    perms = [tuple()]
+    for elem in sequence:
+        next_perms = []
+        for perm in perms:
+            for next_perm in _add_element(perm, elem):
+                next_perms.append(next_perm)
+        perms = next_perms
+    return perms
+
+
+def _add_element(sequence, elem):
+    for i in range(len(sequence) + 1): # insert elem in every position
+        copy = list(sequence)
+        copy.insert(i, elem)
+        yield copy
