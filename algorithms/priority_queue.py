7f150e919a2774e43c2f44d0458cb63304f7d862
Caleb Perkins <calebperkins@users.noreply.github.com>
2026-04-15 23:53:44 -0700

    Typecheck priority queue

--- algorithms/priority_queue.py
+++ algorithms/priority_queue.py
@@ -6,17 +6,17 @@ T = typing.TypeVar("T")
 
 class PriorityQueue(typing.Generic[T]):
     def __init__(self):
-        self._heap: list[T] = []
+        self._heap: list[tuple[int, T]] = []
 
-    def push(self, item: T):
-        heapq.heappush(self._heap, item)
+    def push(self, item: T, priority: int):
+        heapq.heappush(self._heap, (priority, item))
 
     def pop(self) -> T:
-        return heapq.heappop(self._heap)
+        return heapq.heappop(self._heap)[1]
 
     def peek(self) -> T | None:
         if self._heap:
-            return self._heap[0]
+            return self._heap[0][1]
 
     def __bool__(self) -> bool:
         return bool(self._heap)
