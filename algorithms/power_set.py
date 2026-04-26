7da69f3ba92d0fe82268c0b10ff0c8a2616b0924
Caleb Perkins <calebperkins@users.noreply.github.com>
2026-04-25 21:02:28 -0700

    type power_set

--- algorithms/power_set.py
+++ algorithms/power_set.py
@@ -1,10 +1,15 @@
+from collections.abc import Collection, Iterator
+from typing import TypeVar
+
+T = TypeVar("T")
+
 # Generate the set of all subsets (the power set).
 
 # There are 2^N members in the set. Each integer in that range is a bit mask
 # definining which members of the set are to be included.
 
 
-def power_set(S):
-    n = len(S)
+def power_set(coll: Collection[T]) -> Iterator[list[T]]:
+    n = len(coll)
     for mask in range(pow(2, n)):
-        yield [elem for i, elem in enumerate(S) if (mask >> i) & 1]
+        yield [elem for i, elem in enumerate(coll) if (mask >> i) & 1]
